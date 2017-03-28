cd2737de7f0d3d779871222136b1db4fa9de9789
Noel Bao <noeleon930@gmail.com>
2017-03-28 08:14:21 +0800

    ver 1

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const path = require('path')
+
+const express = require('express')
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(process.env.PORT || 5000)
